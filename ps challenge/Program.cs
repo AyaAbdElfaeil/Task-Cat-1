f290cdf3dcc49d0ca572eb8b7387c61dbdcff751
AyaAbdElfaeil <fadelm676@gmail.com>
2025-12-15 20:24:27 +0200

    Add files via upload

---
+++ ps challenge/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+   
+    static void Main() { 
+   
+        List<string> words = new List<string>  ();
+        words.Add("code");
+        words.Add("backend");
+        words.Add("ai");
+        words.Add("circle");
+        string shortest = words[0];
+        for (int i=0;i<words.Count;i++)
+        {if (words[i].Length < shortest.Length)
+            {
+                shortest = words[i];
+                
+            }
+        }
+        Console.WriteLine(shortest);
+  
+    }
+}
